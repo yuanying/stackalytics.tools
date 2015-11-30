f3715a26916fe9b5cefb65c389c206dc5f20d44f
OTSUKA, Yuanying <yuanying@fraction.jp>
2015-11-30 14:29:18 +0900

    Add footer style

--- chart.rb
+++ chart.rb
@@ -110,6 +110,28 @@ __END__
   .chart_off {
     color: #cccccc;
   }
+  /* Sticky footer styles
+  -------------------------------------------------- */
+  html {
+    position: relative;
+    min-height: 100%;
+  }
+  body {
+    /* Margin bottom by footer height */
+    margin-bottom: 60px;
+  }
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    /* Set the fixed height of the footer here */
+    height: 60px;
+    background-color: #f5f5f5;
+  }
+  .footer .container {
+    padding: 1em;
+    padding-left: 3em;
+  }
   </style>
   <script>
   var datasets = [
